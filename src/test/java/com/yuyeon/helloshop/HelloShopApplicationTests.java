bf136caf493cb4ce3fe6e1cba8be44447dfada71
baeyuyeon <31380511+baeyuyeon@users.noreply.github.com>
2023-08-16 14:40:59 +0900

    Initial commit

---
+++ src/test/java/com/yuyeon/helloshop/HelloShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyeon.helloshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
